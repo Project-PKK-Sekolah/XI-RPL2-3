37422170b9ff505af71e63b4851bd7de4240d405
IrhamGhani22 <irhamag22@gmail.com>
2020-03-25 18:51:28 +0700

    Tampilan Fitur Gudang dan Tambah Barang

--- src/tugas/Main.java
+++ src/tugas/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/tugas/View/v_halamanUtama.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/tugas/View/v_tambah.fxml"));
         
         Scene scene = new Scene(root);
         
